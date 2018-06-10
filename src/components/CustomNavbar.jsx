b1903b29711af9b9de9c3e386dc55452886e482f
fahad-zakir <fahadzakir81@gmail.com>
2018-06-10 08:25:48 -0400

    added navbar styles in customnavbarcss

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './CustomNavbar.css';
 
 class CustomNavbar extends Component {
     render() {
