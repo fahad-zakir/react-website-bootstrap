a566846c04be4254bf3acee46f06f6b225681fef
fahad-zakir <fahadzakir81@gmail.com>
2018-06-10 08:19:35 -0400

    added navbar component after setting it up with bootstrap settings

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -14,14 +14,14 @@ class CustomNavbar extends Component {
                 </ Navbar.Header>
                 <Navbar.Collapse>
                     <Nav pullRight>
-                        <NavItem eventKey={1} componentClass={Link} href="/" to="/">
+                        <NavItem eventKey={1} componentClass={Link} to="/">
                             Home
                         </NavItem>
-                        <NavItem eventKey={2} componentClass={Link} href="/" to="/">
-                            Home
+                        <NavItem eventKey={2} componentClass={Link} to="/about">
+                            About
                         </NavItem>
-                        <NavItem eventKey={3} componentClass={Link} href="/" to="/">
-                            Home
+                        <NavItem eventKey={3} componentClass={Link} to="/news">
+                            News
                         </NavItem>
                     </Nav>
                 </Navbar.Collapse>
